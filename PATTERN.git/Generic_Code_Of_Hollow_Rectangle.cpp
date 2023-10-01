bbad74bbbf8ef5efda54ebd21b28e3caf1032e8a
VIOLA <146161086+tecnicoviola@users.noreply.github.com>
2023-10-01 19:17:20 +0530

    Add files via upload

---
+++ PATTERN.git/Generic_Code_Of_Hollow_Rectangle.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    
+    int rowcount, colcount;
+    cin>>rowcount;
+    cin>>colcount;
+    
+    //hollow rectangle
+    for(int row = 0; )
+}
