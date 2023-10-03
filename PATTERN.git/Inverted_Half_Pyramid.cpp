ed851d4c327ae59167d46cb8834c9bfa8b3b3d7c
VIOLA <146161086+tecnicoviola@users.noreply.github.com>
2023-10-03 19:19:00 +0530

    Add files via upload

---
+++ PATTERN.git/Inverted_Half_Pyramid.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    int i , j;
+
+    for(i = n ; i >= 1 ; i--){
+
+    for(j = 1; j<=i; j++){
+    cout<<" * ";
+}
+cout<<endl;
+}
+}
