c6f65464b62d87eb40b28936b52464f64686bbf2
VIOLA <146161086+tecnicoviola@users.noreply.github.com>
2023-10-05 19:22:21 +0530

    Add files via upload

---
+++ PATTERN.git/Print_Counting_In_Pattern.cpp
@@ -0,0 +1,24 @@
+                // 1
+                // 2 3
+                // 3 4 5
+                // 4 5 6 7
+                // 5 6 7 8 9
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    int i, start,  j;
+
+    for(i = n ; i >= 1 ; i--){
+
+        start = i+1;
+
+    for(j = 1; j<=i; j++){
+    cout<<" * ";
+}
+cout<<endl;
+}
+}
