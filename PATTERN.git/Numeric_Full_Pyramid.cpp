9a54e547453a671fb84b41cfd3b9637a08f402fe
VIOLA <146161086+tecnicoviola@users.noreply.github.com>
2023-10-07 19:09:02 +0530

    Add files via upload

---
+++ PATTERN.git/Numeric_Full_Pyramid.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    
+    int  i, j,k, n;
+    cin>>n;
+
+    for(i=1; i<=10; i++){
+        
+        for(j=1; j<i; j++){
+            cout<<j;
+        }
+
+    // for(k=i; k>=1; k--){
+    //     cout<<k;
+    // }
+       cout<<endl;
+}
+}
