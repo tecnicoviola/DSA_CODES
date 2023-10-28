4a09262e0644d9b23736793ba8603ccce9a11544
VIOLA <146161086+tecnicoviola@users.noreply.github.com>
2023-10-28 19:57:52 +0530

    DAY 27

---
+++ W4_SEARCH_N_SORT/Operator2.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include<algorithm>
+#include<vector>
+#include<map>
+using namespace std;
+
+bool cmp(char first, char second) {
+        return first > second;
+}
+
+bool compare(int a, int b) {
+        cout << "First number is: " << a ;
+        cout << " Second number is: " << b << endl << endl; 
+        return a > b;
+}
+
+int main() {
+    map<int, char> meraMap;
+        //insertion
+        meraMap[0] = 'a';
+        meraMap[1] = 'b';
+        meraMap[25] = 'z';
+
+        cout << "Your ans is: " << meraMap[1] <<endl;
+}
