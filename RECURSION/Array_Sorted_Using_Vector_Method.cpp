54739235e29ae1ff3315f23e7ddae0f3d1d875cd
VIOLA <146161086+tecnicoviola@users.noreply.github.com>
2023-12-15 20:13:37 +0530

    Update Array_Sorted_Using_Vector_Method.cpp

---
+++ RECURSION/Array_Sorted_Using_Vector_Method.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include<vector>
+using namespace std;
+
+bool checkSorted(vector<int> &arr, int& n, int i) {
+  //base case
+  if(i == n-1) {
+    return true;
+  }
+
+  if(arr[i+1] <= arr[i])
+    return false;
+    bool ans = checkSorted(arr, n, i+1);
+    return ans;
+
+}
+
+int main() {
+  vector<int> v{4,3,1,2,4,4};
+  int n = v.size();
+  int i = 0;
+  bool isSorted = checkSorted(v, n, i);
+
+  if(isSorted) {
+    cout << "Array is sorted" << endl;
+  }
+  else {
+    cout << "Array is not sorted" << endl;
+  }
+
+  return 0;
+}
