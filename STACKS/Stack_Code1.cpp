1b6114377a7c4d104de4ecfe730c04127781cd9e
VIOLA <146161086+tecnicoviola@users.noreply.github.com>
2023-12-18 22:44:16 +0530

    Update Stack_Code1.cpp

---
+++ STACKS/Stack_Code1.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+
+int main(){
+    //creation of stack
+    stack<int>st;
+
+    //insertion
+    st.push(10);
+    st.push(20);
+    st.push(30);
+    st.push(40);
+
+    //remove
+    st.pop();
+
+    //check element on top
+    cout<<"element  on top is"<<st.top()<<endl;
+
+    //size
+    cout<<"size of stack is:"<<st.size()<<endl;
+
+     if(st.empty()){
+        cout<<"stack is empty"<<endl;
+     }
+     else
+        cout<<"stack is not empty"<<endl;
+
+
+    return 0;
+
+}
