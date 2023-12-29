a656191a8025078cd6993b46a1969c067d10b1e3
VIOLA <146161086+tecnicoviola@users.noreply.github.com>
2023-12-29 21:33:42 +0530

    DAY 72

---
+++ TREE/Pre_Order_Traversal.cpp
@@ -0,0 +1,47 @@
+//NLR
+#include <iostream>
+#include<queue>
+using namespace std;
+
+class Node{
+	public:
+	int data;
+	Node* left;
+	Node* right;
+
+	Node(int data) {
+		this->data = data;
+		left = NULL;
+		right = NULL;
+	}
+};
+
+Node* buildTree() {
+	int data; 
+	cout << "Enter the data: " << endl;
+	cin >> data;
+
+	if(data == -1) {
+		return NULL;
+	}
+}
+
+void preorderTraversal(Node* root) {
+	//base case
+	if(root == NULL) {
+		return ;
+	}
+
+	//NLR
+	cout << root -> data << " ";
+	preorderTraversal(root->left);
+	preorderTraversal(root->right);
+}
+int main() {
+    Node* root = buildTree();
+
+    cout << "Preorder Traversal: ";
+    preorderTraversal(root);
+
+    return 0;
+}
