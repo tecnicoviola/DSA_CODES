22d0cb30c079be6aad00b51a0f68b4184dfd184f
VIOLA <146161086+tecnicoviola@users.noreply.github.com>
2023-12-30 21:57:49 +0530

    DAY 73

---
+++ TREE/Post_Order_Traversal.cpp
@@ -0,0 +1,45 @@
+//LRN
+#include <iostream>
+#include<queue>
+using namespace std;
+
+class Node{
+	public:
+	int data;
+	Node* left;
+	Node* right;
+
+	Node(int data) {
+		this->data = data;
+		left = NULL;
+		right = NULL;
+	}
+};
+
+Node* buildTree() {
+	int data; 
+	cout << "Enter the data: " << endl;
+	cin >> data;
+
+	if(data == -1) {
+		return NULL;
+	}
+}
+
+void postorderTraversal(Node* root) {
+	if(root == NULL)
+		return;
+
+	//LRN
+	postorderTraversal(root->left);
+	postorderTraversal(root->right);
+	cout << root -> data << " ";
+}
+int main() {
+    Node* root = buildTree();
+
+    cout << "Postorder Traversal: ";
+    postorderTraversal(root);
+
+    return 0;
+}
