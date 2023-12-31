86be3f571a8ab7892c9fec6a0fa22427300b4fa5
VIOLA <146161086+tecnicoviola@users.noreply.github.com>
2023-12-31 21:01:53 +0530

    DAY 74

---
+++ TREE/Level_Order_Traversal.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include<queue>
+using namespace std;
+
+class Node{
+	public:
+	int data;
+	Node* left;
+	Node* right;
+
+	Node(int data) {
+		this->data = data;
+		left = NULL;
+		right = NULL;
+	}
+};
+
+Node* buildTree() {
+	int data; 
+	cout << "Enter the data: " << endl;
+	cin >> data;
+
+	if(data == -1) {
+		return NULL;
+	}
+}
+
+void levelOrderTraversal(Node* root ) {
+	queue<Node*> q;
+	//initially
+	q.push(root);
+	q.push(NULL);
+
+	while(!q.empty()) {
+		//A
+		Node* temp = q.front();
+		//B
+		q.pop();
+		
+		if(temp == NULL) {
+			cout << endl;
+			if(!q.empty()) {
+				q.push(NULL);
+			}	
+		}
+		else {
+			//C
+			cout << temp->data << " ";
+			//Dclass solution{
+		}
+	}
+}
